e9ba640073cf6be40f8134be0bea6bea2a1cd00f
Disha Patel <disharpatel89@gmail.com>
2019-09-10 20:24:28 -0400

    master

---
+++ XmlToJson/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace XmlToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("https://www.w3schools.com/xml/note.xml");
+
+            string json = JsonConvert.SerializeXmlNode(xmlDocument);
+            Console.WriteLine(json);
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "test.json")))
+            {
+               
+                    outputFile.WriteLine(json);
+            }
+        }
+    }
+}
